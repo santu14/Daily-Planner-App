6deba3b3fbf510f79ee4ed1e7d2ec53eff470c73
Santiago Solana <santiago.sjs@gmail.com>
2020-07-16 19:16:55 -0400

    Edited read me to be more descriptive/ minor clean up

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 # Daily-Planner-App
-This is a simple calendar application which allows the user to save different tasks and event by the hour of the day. this app features dynamically updated HTML and CSS powered by jQuery and uses the Moment.js library to work with date and time.
+This app features dynamically updated HTML and CSS powered by jQuery and uses the Moment.js library to work with date and time.
 
----
+# Use
+
+### Save different tasks and events by the hour of the day!
+- Click on colored textarea corresponding to desired time
+- Type whichever event or task you would like to acomplish
+- Simply click on the purple button to the right of text box to save!
+#### Note: Different colors indicate past present and future
+
+<img src="Assets/screenshot.jpg" width ="600"></img>
+
+- - -
 [Daily Planer App](https://santu14.github.io/Daily-Planner-App/)
--- script.js
+++ script.js
@@ -1,111 +1,110 @@
 
 $(document).ready(function () {
-    
-    
+
+
     var now = moment();
     var currentHour = parseInt(now.format("H"));
 
     var plannerHours = [
-        displayData = ["7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM"],
-        hourData = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+        displayData = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM"],
+        hourData = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
     ];
 
-    var todoDisplay = ["", "", "", "", "", "", "", "", "", "", "", "", "" ];
+    var savedTextArray = ["", "", "", "", "", "", "", "", "", "", "", "", ""];
     
+
     $("#currentDay").text(moment(now).format("dddd MMMM Do"));
-    
+
+
     function displayTimeblocks(text, data) {
         var mainDisplay = $("tbody");
-        
-        
+
+
         for (i = 0; i < text.length; i++) {
 
             var newRow = $("<tr class='time-block'>");
             newRow.attr("data-type", data[i]);
             var timeData = parseInt(newRow.attr("data-type"));
 
-            console.log(newRow.attr("data-type"));
-            
+
             var timeDisplay = $("<td class='hour'>");
             timeDisplay.text(text[i]);
 
             var textarea = $("<textarea class='textarea" + data[i] + "' rows='3' cols='60'>");
             var textareaDisplay = $("<td class='description '>");
             textareaDisplay.append(textarea);
-            
-            var saveBtnDisplay = $("<td class='saveBtn btn' data-type="+ i +">");
+
+            var saveBtnDisplay = $("<td class='saveBtn btn' data-type=" + i + ">");
             saveBtnDisplay.append($("<i class='fa fa-floppy-o fa-3x'>"));
-            
+
             newRow.append(timeDisplay, textareaDisplay, saveBtnDisplay);
             mainDisplay.append(newRow);
-            
+
             checkTime(timeData, textarea);
-            displayStoredText (textarea);
+            displayStoredText(textarea);
 
         }
 
-        console.log(todoDisplay);
+        console.log(savedTextArray);
+    }
+
+    
+    function saveTextAreaOnClick() {
+        $(".saveBtn").on("click", function () {
+            var currentTimeData = $(this).parent().attr("data-type");
+            var clickedTextarea = $(".textarea" + currentTimeData);
+            var rowToArrayCorrelation = $(this).attr("data-type");
+            
+            savedTextArray[rowToArrayCorrelation] = clickedTextarea.val();
+    
+            storeTextInput();
+        });
     }
     
-    displayTimeblocks(plannerHours[0], plannerHours[1]);
 
-
-    $(".saveBtn").on("click", function(){
-        var currentTimeData = $(this).parent().attr("data-type");
-        var clickedTextarea = $(".textarea"+ currentTimeData);
-        var lineArrayCorrelation = $(this).attr("data-type");
-
-        todoDisplay[lineArrayCorrelation] = clickedTextarea.val();
-
-
-        console.log(currentTimeData);
-        console.log(todoDisplay[currentTimeData]);
-        console.log(todoDisplay );
-        console.log("todoDisplay" );
-
-
-        storeTextInput();
-
-    })
-
-
-
-    function displayStoredText (textarea) {
-
+    function clearAllOnClick (){
+        $("#clear-btn").on("click", function() {
+            localStorage.clear();
+            location.reload();
+        })
+    }
+    
+    
+    function displayStoredText(textarea) {
+        
         var storedTextInput = JSON.parse(localStorage.getItem("savedtext"));
-
-        if (storedTextInput != null){
-            todoDisplay = storedTextInput;
-            textarea.val(todoDisplay[i]);
-           
+        
+        if (storedTextInput != null) {
+            savedTextArray = storedTextInput;
+            textarea.val(savedTextArray[i]);
+            
         };
     };
+    
 
-
-    function checkTime(timeData, textarea){
+    function checkTime(timeData, textarea) {
         
         if (timeData === currentHour) {
             textarea.addClass("present");
-
+            
         } else if (timeData < currentHour) {
             textarea.addClass("past");
-
+            
         } else {
             textarea.addClass("future");
-
+            
         };
     }
-
-
-
-    function storeTextInput(){
-        localStorage.setItem("savedtext", JSON.stringify(todoDisplay));
+    
+    
+    function storeTextInput() {
+        localStorage.setItem("savedtext", JSON.stringify(savedTextArray));
     };
     
-    
-    console.log(plannerHours);
-    console.log(typeof currentHour);
-    
-    
 
+    displayTimeblocks(plannerHours[0], plannerHours[1]);
+    saveTextAreaOnClick();
+    clearAllOnClick ();
+    
+    
 });
